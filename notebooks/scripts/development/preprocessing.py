fe21739cab1569e8acf7bbd7e40058f74e1bc42d
AndreiT <andrei.trofin7@gmail.com>
2022-08-12 23:55:10 +0300

    3. preprocessing early stage - email removal

--- notebooks/scripts/development/preprocessing.py
+++ notebooks/scripts/development/preprocessing.py
@@ -1,5 +1,8 @@
+import re
+
 import pandas as pd
 from chardet.universaldetector import UniversalDetector
+from contractions import contractions_dict
 
 """
 Initial data verification
@@ -31,7 +34,7 @@ Data preprocessing functions
 def get_avg_word_len(text):
     """
     Function which returns the average word length within a paragraph.
-    :param text: the actual text
+    :param text: The text to search through.
     :return: the average word length from the text
     """
     words = text.split()
@@ -42,23 +45,69 @@ def get_avg_word_len(text):
     return total_word_len/len(words)
 
 
-def get_number_of_hashtags(text):
+def get_hashtags_count(text):
     """
-    Function which return the number of hashtags in a text. It will not count single #, it has to be followed by other
+    Function which returns the number of hashtags in a text. It will not count single #, it has to be followed by other
     characters.
-    :param text: the actual text
+    :param text: The text to search through.
     :return: the number of hashtags in the text
     """
     return len([word for word in text.split() if len(word) > 1 and word.startswith("#")])
 
 
-def get_number_of_mentions(text):
+def get_mentions_count(text):
     """
-    Function which return the number of mentions in a text. It will not count single @, it has to be followed by other
+    Function which returns the number of mentions in a text. It will not count single @, it has to be followed by other
     characters.
-    :param text: the actual text
+    :param text: The text to search through.
     :return: the number of mentions in the text
     """
     return len([word for word in text.split() if len(word) > 1 and word.startswith("@")])
 
 
+def get_upper_case_count(text):
+    """
+    Function which return the number of uppercase words with more than 3 characters in a text.
+    :param text: The text to search through.
+    :return: the number of uppercase words with a length bigger than 3 in the text
+    """
+    return len([word for word in text.split() if len(word) > 3 and word.isupper()])
+
+
+def expand_contractions(text):
+    """
+    Function which expands the words in a text given a static dictionary.
+    The assumption is that the text is in lowercase.
+    :param text: The text to search through.
+    :return:
+    """
+    my_contractions_dict = {k.lower(): v.lower() for k, v in contractions_dict.items()}
+    # Add some contractions that are not in the library
+    my_contractions_dict.update({' u ': ' you ', ' n ': ' and ', " 'n ": ' and ', ' w ': ' with ', ' w/ ': ' with ',
+                                 ' ur ': ' your ', ' nah ': ' no '})
+    if type(text) is str:
+        for key in my_contractions_dict.keys():
+            value = my_contractions_dict[key]
+            text = text.replace(key, value)
+    return text
+
+
+def get_email_count(text):
+    """
+    Get the number of emails in the text.
+    :param text: The text to search through.
+    :return: The number of email mentions in the text
+    """
+    mail_regex = r'([a-zA-Z0-9+._-]+@[a-zA-Z0-9._-]+\.[a-zA-Z0-9_-]+)'
+    return len(re.findall(mail_regex, text))
+
+
+def remove_emails(text):
+    """
+    Remove the emails from text
+    :param text: The text to search through.
+    :return: The text without any email that was initially in.
+    """
+    mail_regex = r'([a-zA-Z0-9+._-]+@[a-zA-Z0-9._-]+\.[a-zA-Z0-9_-]+)'
+
+    return re.sub(mail_regex, '', text)
--- notebooks/scripts/test/test_preprocessing.py
+++ notebooks/scripts/test/test_preprocessing.py
@@ -1,5 +1,6 @@
 from unittest import TestCase
-from preprocessing import get_avg_word_len, get_number_of_hashtags, get_number_of_mentions
+from preprocessing import get_avg_word_len, get_hashtags_count, \
+    get_mentions_count, get_upper_case_count, expand_contractions, get_email_count, remove_emails
 
 
 class TestPreprocessingFunctions(TestCase):
@@ -22,8 +23,8 @@ class TestPreprocessingFunctions(TestCase):
         text2 = "#mine #cool #hashtag #queen slay wow # damn"
 
         # When
-        count1 = get_number_of_hashtags(text1)
-        count2 = get_number_of_hashtags(text2)
+        count1 = get_hashtags_count(text1)
+        count2 = get_hashtags_count(text2)
 
         # Then
         self.assertEqual(count1, 4)
@@ -35,9 +36,52 @@ class TestPreprocessingFunctions(TestCase):
         text2 = "@mine #cool @hashtag #queen slay wow @ damn "
 
         # When
-        count1 = get_number_of_mentions(text1)
-        count2 = get_number_of_mentions(text2)
+        count1 = get_mentions_count(text1)
+        count2 = get_mentions_count(text2)
 
         # Then
         self.assertEqual(count1, 4)
         self.assertEqual(count2, 2)
+
+    def test_get_number_of_uppercase_words(self):
+        # Given
+        text = "HOLD ON I have received this message from James SRL and it's AMAZING!"
+
+        # When
+        count = get_upper_case_count(text)
+
+        # Then
+        self.assertEqual(count, 2)
+
+    def test_expand_contractions(self):
+        # Given
+        text = "I'll be right back w/ u I can't wait to see you. I shan't forget"
+        text = text.lower()
+
+        # When
+        expansion = expand_contractions(text)
+
+        # Then
+        self.assertEqual(expansion, "i will be right back with you i cannot wait to see you. i shall not forget")
+
+    def test_get_email_count(self):
+        # Given
+        text = "I wanna check with jo@bee.com and michael@wawawawa.brough and perhaps jimmy-1212@me.com and " \
+               "?+-231aa@ha.do wahoo@me?com"
+
+        # When
+        count = get_email_count(text)
+
+        # Then
+        self.assertEqual(4, count)
+
+    def test_remove_emails(self):
+        # Given
+        text = "I wanna check with jo@bee.com and michael@wawawawa.brough and perhaps jimmy-1212@me.com and " \
+               "?+-231aa@ha.do wahoo@me?com"
+
+        # When
+        new_text = remove_emails(text)
+
+        # Then
+        self.assertEqual("I wanna check with  and  and perhaps  and  wahoo@me?com", new_text)
