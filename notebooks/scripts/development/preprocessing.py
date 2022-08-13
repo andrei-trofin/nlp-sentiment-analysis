09dbc665670979e8762c3f2856d79b2933d742c4
AndreiT <andrei.trofin7@gmail.com>
2022-08-13 22:51:04 +0300

    4. preprocessing middle stage - added extra steps

--- notebooks/scripts/development/preprocessing.py
+++ notebooks/scripts/development/preprocessing.py
@@ -1,8 +1,8 @@
 import re
 
-import pandas as pd
 from chardet.universaldetector import UniversalDetector
 from contractions import contractions_dict
+from unicodedata import normalize
 
 """
 Initial data verification
@@ -12,8 +12,8 @@ Initial data verification
 def get_file_encoding(file_path):
     """
     Function that outputs the most probable encoding for a file at a file path.
-    :param file_path: the path of the file that needs encoding check
-    :return: the file encoding that the library is most confident that it is correct and the confidence
+    :param file_path: The path of the file that needs encoding check.
+    :return: The file encoding that the library is most confident that it is correct and the confidence.
     """
     detector = UniversalDetector()
     with open(file_path, "rb") as file:
@@ -35,7 +35,7 @@ def get_avg_word_len(text):
     """
     Function which returns the average word length within a paragraph.
     :param text: The text to search through.
-    :return: the average word length from the text
+    :return: The average word length from the text.
     """
     words = text.split()
     total_word_len = 0
@@ -50,26 +50,16 @@ def get_hashtags_count(text):
     Function which returns the number of hashtags in a text. It will not count single #, it has to be followed by other
     characters.
     :param text: The text to search through.
-    :return: the number of hashtags in the text
+    :return: The number of hashtags in the text.
     """
     return len([word for word in text.split() if len(word) > 1 and word.startswith("#")])
-
-
-def get_mentions_count(text):
-    """
-    Function which returns the number of mentions in a text. It will not count single @, it has to be followed by other
-    characters.
-    :param text: The text to search through.
-    :return: the number of mentions in the text
-    """
-    return len([word for word in text.split() if len(word) > 1 and word.startswith("@")])
 
 
 def get_upper_case_count(text):
     """
     Function which return the number of uppercase words with more than 3 characters in a text.
     :param text: The text to search through.
-    :return: the number of uppercase words with a length bigger than 3 in the text
+    :return: The number of uppercase words with a length bigger than 3 in the text.
     """
     return len([word for word in text.split() if len(word) > 3 and word.isupper()])
 
@@ -78,7 +68,7 @@ def expand_contractions(text):
     """
     Function which expands the words in a text given a static dictionary.
     The assumption is that the text is in lowercase.
-    :param text: The text to search through.
+    :param text: The text to modify.
     :return:
     """
     my_contractions_dict = {k.lower(): v.lower() for k, v in contractions_dict.items()}
@@ -96,7 +86,7 @@ def get_email_count(text):
     """
     Get the number of emails in the text.
     :param text: The text to search through.
-    :return: The number of email mentions in the text
+    :return: The number of emails in the text.
     """
     mail_regex = r'([a-zA-Z0-9+._-]+@[a-zA-Z0-9._-]+\.[a-zA-Z0-9_-]+)'
     return len(re.findall(mail_regex, text))
@@ -104,10 +94,120 @@ def get_email_count(text):
 
 def remove_emails(text):
     """
-    Remove the emails from text
-    :param text: The text to search through.
+    Remove the emails from text.
+    :param text: The text to modify.
     :return: The text without any email that was initially in.
     """
     mail_regex = r'([a-zA-Z0-9+._-]+@[a-zA-Z0-9._-]+\.[a-zA-Z0-9_-]+)'
 
     return re.sub(mail_regex, '', text)
+
+
+def get_url_count(text):
+    """
+    Get the number of URLs in the text.
+    :param text: The text to search through.
+    :return: The number of URLs in the text.
+    """
+    url_regex = r'(http|ftp|https):\/\/([\w\-_]+(?:(?:\.[\w\-_]+)+))([\w\-\.,@?^=%&:/~\+#]*[\w\-\@?^=%&/~\+#])?'
+
+    return len(re.findall(url_regex, text))
+
+
+def remove_urls(text):
+    """
+    Remove the URLs from text.
+    :param text: The text to modify.
+    :return: The text without any URL that was initially in.
+    """
+    url_regex = r'(http|ftp|https):\/\/([\w\-_]+(?:(?:\.[\w\-_]+)+))([\w\-\.,@?^=%&:/~\+#]*[\w\-\@?^=%&/~\+#])?'
+
+    return re.sub(url_regex, '', text)
+
+
+def remove_retweet(text):
+    """
+    Function which removes the rt part of the message if it is a retweet.
+    :param text: The text to modify.
+    :return: The text without the retweet key.
+    """
+    retweet_regex = r'( |^)rt( @)'
+    retweet_keep_regex = r'\1\2'
+
+    return re.sub(retweet_regex, retweet_keep_regex, text)
+
+
+def get_mention_count(text):
+    """
+    Get the number of mentions in the text.
+    :param text: The text to search through.
+    :return: The number of mentions in the text
+    """
+    mention_regex = r'[ \.,\\\/;]@\w+[ \.,\\\/;]'
+
+    return len(re.findall(mention_regex, text))
+
+
+def remove_mentions(text):
+    """
+    Function which removes the mentions from the message.
+    :param text: The text to modify
+    :return: The text without the mentions.
+    """
+    mention_regex = r'([ \.,\\\/;])@\w+([ \.,\\\/;])'
+    mention_keep_regex = r'\1\2'
+
+    return re.sub(mention_regex, mention_keep_regex, text)
+
+
+def is_reply(text):
+    """
+    Functions which returns whether a tweet is a reply to someone or not
+    :param text: The text to search through.
+    :return: 1 if the tweet is a reply, 0 if not.
+    """
+    mention_regex = r'^@\w+[ \.,\\\/;]'
+
+    return len(re.findall(mention_regex, text))
+
+
+def remove_reply_target(text):
+    """
+    Function which removes the reply target from the message.
+    :param text: The text to modify.
+    :return: The text without the reply target.
+    """
+    mention_regex = r'^@\w+([ \.,\\\/;])'
+    mention_keep_regex = r'\1'
+
+    return re.sub(mention_regex, mention_keep_regex, text)
+
+
+def remove_accents(text):
+    """
+    Function which removes the accents on words from the message.
+    :param text: The text to modify.
+    :return: The text without the accents on words.
+    """
+
+    return normalize('NFKD', text).encode('ascii', 'ignore').decode('ascii', 'ignore')
+
+
+def remove_special_characters(text):
+    """
+    Function which removes all the special characters, besides those used for sentences (?.:!-) from text.
+    :param text: The text to modify.
+    :return: Text without any special characters besides sentence separators. Special characters are replaced by space.
+    """
+    special_chars_regex = r'[^A-Za-z0-9 -\.!?:]+'
+    return re.sub(special_chars_regex, ' ', text)
+
+
+def remove_extra_spaces(text):
+    """
+    Function which removes all the extra spaces from text.
+    :param text: The text to modify.
+    :return: Text with one space between each word.
+    """
+    extra_space_regex = r' +'
+    return re.sub(extra_space_regex, ' ', text)
--- notebooks/scripts/test/test_preprocessing.py
+++ notebooks/scripts/test/test_preprocessing.py
@@ -1,6 +1,7 @@
 from unittest import TestCase
-from preprocessing import get_avg_word_len, get_hashtags_count, \
-    get_mentions_count, get_upper_case_count, expand_contractions, get_email_count, remove_emails
+from preprocessing import get_avg_word_len, get_hashtags_count, get_upper_case_count, \
+    expand_contractions, get_email_count, remove_emails, get_url_count, remove_urls, remove_retweet, get_mention_count,\
+    remove_mentions, is_reply, remove_reply_target, remove_accents, remove_special_characters, remove_extra_spaces
 
 
 class TestPreprocessingFunctions(TestCase):
@@ -29,19 +30,6 @@ class TestPreprocessingFunctions(TestCase):
         # Then
         self.assertEqual(count1, 4)
         self.assertEqual(count2, 4)
-
-    def test_get_number_of_mentions(self):
-        # Given
-        text1 = "this is @great I feel so cool @blessed #yay #wooho #whoops @mee @@@"
-        text2 = "@mine #cool @hashtag #queen slay wow @ damn "
-
-        # When
-        count1 = get_mentions_count(text1)
-        count2 = get_mentions_count(text2)
-
-        # Then
-        self.assertEqual(count1, 4)
-        self.assertEqual(count2, 2)
 
     def test_get_number_of_uppercase_words(self):
         # Given
@@ -85,3 +73,107 @@ class TestPreprocessingFunctions(TestCase):
 
         # Then
         self.assertEqual("I wanna check with  and  and perhaps  and  wahoo@me?com", new_text)
+
+    def test_get_url_count(self):
+        # Given
+        text = "@switchfoot http://twitpic.com/2y1zl - awww, t... httv://www.me.com ftp://ro.know.me/"
+
+        # When
+        count = get_url_count(text)
+
+        # Then
+        self.assertEqual(2, count)
+
+    def test_remove_urls(self):
+        # Given
+        text = "@switchfoot http://twitpic.com/2y1zl - awww, t... httv://www.me.com ftp://ro.know.me/"
+
+        # When
+        new_text = remove_urls(text)
+
+        # Then
+        self.assertEqual("@switchfoot  - awww, t... httv://www.me.com ", new_text)
+
+    def test_remove_retweet(self):
+        # Given
+        text = "rt @bro come here and tell me rt now where you at I wanna know if art is good or not. rt @romeo ha"
+
+        # When
+        new_text = remove_retweet(text)
+
+        # Then
+        self.assertEqual(" @bro come here and tell me rt now where you at I wanna know if art is good or not."
+                         "  @romeo ha", new_text)
+
+    def test_get_mention_count(self):
+        # Given
+        text = "@switchfoot let's talk to @james and @michale and @bee222 to see me @ the pub @@@"
+
+        # When
+        count = get_mention_count(text)
+
+        # Then
+        self.assertEqual(3, count)
+
+    def test_remove_mentions(self):
+        # Given
+        text = "@switchfoot let's talk to @james and @michale and @bee222 to see me @ the pub @@@"
+
+        # When
+        count = remove_mentions(text)
+
+        # Then
+        self.assertEqual("@switchfoot let's talk to  and  and  to see me @ the pub @@@", count)
+
+    def test_is_reply(self):
+        # Given
+        text1 = "@switchfoot let's talk to @james and @michale and @bee222 to see me @ the pub @@@"
+        text2 = "bamabam @me hey yo hey"
+
+        # When
+        is_reply_flag1 = is_reply(text1)
+        is_reply_flag2 = is_reply(text2)
+
+        # Then
+        self.assertEqual(1, is_reply_flag1)
+        self.assertEqual(0, is_reply_flag2)
+
+    def test_remove_reply_target(self):
+        # Given
+        text = "@switchfoot let's talk to @james and @michale and @bee222 to see me @ the pub @@@"
+
+        # When
+        count = remove_reply_target(text)
+
+        # Then
+        self.assertEqual(" let's talk to @james and @michale and @bee222 to see me @ the pub @@@", count)
+
+    def test_remove_accents(self):
+        # Given
+        text = "dà, è, ì, ò, ù, á, é, í, ó, ú, ý, â, ê, î, ô, û, ã, ñ, õ, ä, ë, ï, ö, ü, ÿ, å, ç"
+
+        # When
+        count = remove_accents(text)
+
+        # Then
+        self.assertEqual("da, e, i, o, u, a, e, i, o, u, y, a, e, i, o, u, a, n, o, a, e, i, o, u, y, a, c", count)
+
+    def test_remove_special_characters(self):
+        # Given
+        text = "So cool dude ... hope u can be.so.nice and you+me=great h@h@h@ see ya @@@@ wanna-be"
+
+        # When
+        new_text = remove_special_characters(text)
+
+        # Then
+        self.assertEqual("So cool dude   hope u can be so nice and you me great h h h  see ya   wanna-be", new_text)
+
+    def test_remove_extra_spaces(self):
+        # Given
+        text = "So cool dude   hope u can be so nice and you me great h h h  see ya   wanna-be"
+
+        # When
+        new_text = remove_extra_spaces(text)
+
+        # Then
+        self.assertEqual("So cool dude hope u can be so nice and you me great h h h see ya wanna-be", new_text)
